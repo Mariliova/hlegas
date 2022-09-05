d9b5dea12843c27be755b01ccbdbf14b82b95533
Mariliova <mashuha1698@gmail.com>
2022-09-05 23:15:14 +0300

    btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
-console.log('2322Test');
+/* JavaScript */
+$(document).ready(function () {
+  $('.header-navbar__hamburger').on('click', function () {
+    $(this).toggleClass('active');
+    $('.header-navbar__list').toggleClass('active');
+  });
+});
